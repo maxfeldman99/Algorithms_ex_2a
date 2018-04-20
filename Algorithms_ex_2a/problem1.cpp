dd81dd4c6e75afedb9a84f58c6c01b8b5c3f765e
MAX FELDMAN <maxfeldman99@gmail.com>
2018-04-21 01:13:44 +0300

    done

--- Algorithms_ex_2a/problem1.cpp
+++ Algorithms_ex_2a/problem1.cpp
@@ -6,74 +6,99 @@
 
 #define INF  9999;
 
-//
+
 using namespace std;
 
+int findMedianOfSorted(int *arr1, int *arr2, int size1, int size2, int low, int high);
+int* setArray(int size);
+int define_size();
+void printArray(int *arr,int size);
 
-int createSequence(int *arr, int size);
-int findMedianOfSorted(int *arr1, int *arr2, int size1, int size2);
-
-
-int createSequence(int *arr, int size) {
-	int firstSeqNum = rand() % 100;
-	int signForSeq = rand() % 2;
-	
-	if (signForSeq == 1) {
-		firstSeqNum = firstSeqNum * (-1);
+void printArray(int *arr, int size) {
+	cout << "values of array of size " << size << " are : ";
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << ",";
 	}
-	arr[0] = firstSeqNum;
-
-	for (int i = 1; i < size; i++) {
-		arr[i] = (i+1) ;
-	}
-	return *arr;
+	cout << endl;
 }
 
-int findMedianOfSorted(int *arr1, int *arr2, int size1, int size2) {
-	int low = 0;
-	int high = size1;
-	int partitionA = (low + high / 2);
-	int partitionB = (size1 + size2 + 1) / 2 - partitionA;
+int* setArray(int size) {
+	int val;
+	int *arr;
+	arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		cout << "insert a value to the array " << endl;
+			cin >> val;
+			if (!cin) {
+				cout << "wrong input , input must contain numbers only!" << endl;
+				system("pause");
+				exit(1);
+			}
+		arr[i] = val;
+	}
+	cout << endl;
+	return arr;
+}
+
+int define_size() {
+	int size;
+	cout << "choose size for the array : " << endl;
+	cin >> size;
+	if (!cin) {
+		cout << "wrong input , input must contain numbers only!" << endl;
+		system("pause");
+		exit(1);
+	}
+	cout << endl;
+	return size;
+}
+
+int findMedianOfSorted(int *arr1, int *arr2, int size1, int size2, int low, int high) {
+
+
 	if (size1 > size2) {
-		findMedianOfSorted(arr2, arr1, size2, size1);
+		findMedianOfSorted(arr2, arr1, size2, size1, low, high);
 	}
 
 
-	while (low <= high) {
-		int partitionA = (low + high / 2);
+	else {
+		int partitionA = (low + high) / 2;
 		int partitionB = (size1 + size2 + 1) / 2 - partitionA;
 
 
 		int maxLeftA = arr1[partitionA - 1];
-		if (partitionA == 0) {   //edge case
-			maxLeftA = -INF;
-		}
+
 		int minRightA = arr1[partitionA];
-		if (partitionA == size1) { //edge case
-			maxLeftA = INF;
-		}
 
 		int maxLeftB = arr2[partitionB - 1];
-		if (partitionB == 0) { //edge case
-			maxLeftB = -INF;
-		}
+
 		int minRightB = arr2[partitionB];
-		if (partitionB == size2) { //edge case
-			minRightB = INF;
+
+
+		if (partitionA > size1 - 1) {
+			minRightA = INF;
 		}
+		if (partitionA == 0) {
+			maxLeftA = -INF;
+		}
+
+
+
 		if (maxLeftA <= minRightB && maxLeftB <= minRightA) { // we found the right partition
 			if ((size1 + size2) % 2 == 0) { // if the union of both of the arrays is even
-				return (fmax(maxLeftA, maxLeftB) + fmin(minRightA, minRightB)) / 2;
+				return fmin(fmax(maxLeftA, maxLeftB), fmin(minRightA, minRightB));
 			}
 			else {   // the union of the arrays is odd
 				return fmax(maxLeftA, maxLeftB);
 			}
 		}
-		else if (maxLeftA < minRightB) {  // move partitionA 1 step to the left
-			high = partitionA - 1;
+		else if (maxLeftB > minRightA) {  // move paritionA 1 step to the right
+			findMedianOfSorted(arr1, arr2, size1, size2, partitionA + 1, high);
+			
 		}
-		else {
-			low = partitionA + 1; // move paritionA 1 step to the right
+		else { // move partitionA 1 step to the left
+			
+			findMedianOfSorted(arr1, arr2, size1, size2, low, partitionA - 1);
 		}
 
 	}
@@ -82,26 +107,17 @@ int findMedianOfSorted(int *arr1, int *arr2, int size1, int size2) {
 
 void main() {
 	srand(time(NULL));
-	int *arr1, *arr2;
-	int size1 = 5, size2 = 7;
+	int *arr1, *arr2, size1, size2;
 	int median;
-	arr1 = new int[size1];
-	arr2 = new int[size2];
-	*arr1 = createSequence(arr1, size1);
-	*arr2 = createSequence(arr2, size2);
+	size1 = define_size();
+	arr1 = setArray(size1);
+	size2 = define_size();
+	arr2 =setArray(size2);
+	printArray(arr1, size1);
+	printArray(arr2, size2);
+	
 
-	//int arr1[4] = { 2,3,5,8 };
-	//int arr2[6] = { 10,12,14,16,18,20 };
-
-
-	for (int i = 0; i < size1; i++) {
-		cout << arr1[i] << ",";
-	}
-	cout << " END " << endl;
-	for (int i = 0; i < size2; i++) {
-		cout << arr2[i] << ",";
-	}
-	median = findMedianOfSorted(arr1, arr2, size1, size2);
+	median = findMedianOfSorted(arr1, arr2, size1, size2, 0, size1);
 	cout << " the median is :" << median << endl;
 	system("pause");
 
